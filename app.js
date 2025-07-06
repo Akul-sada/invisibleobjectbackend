560beba533727005056c1eaf51cf5620f297e3ad
Akul-sada <sadananda.sherigara@gmail.com>
2025-07-06 14:22:12 +0530

    Add hello world get request

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 
-
-
 const app = express();
-
+// Middleware to parse JSON
+app.use(express.json());
 // Define pug
 
 app.set('view engine','pug');
 app.set('views', path.join(__dirname,'views'),'views');
+
+// Create hello world get request
+app.get('/', (req, res) => {
+  res.send('Hello World!');});
+
+
 
 
 module.exports = app;
